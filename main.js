8f3af0db7d1571654846d75e01b4e5a3a402a41e
foo-dogsquared <christiangabrielarazas@gmail.com>
2018-06-12 17:02:56 +0800

    Add the capability to accept JSON outside of site

--- main.js
+++ main.js
@@ -1,67 +1,42 @@
 "use strict";
 function main() {
     const LIST_CONTAINER = document.querySelector("#list-container");
+    const SE_LIST = document.querySelector("ul#search-list");
     const SVG = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" height="16"><path d="M505 442.7L405.3 343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 44-79.7 44-128C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7 17l99.7 99.7c9.4 9.4 24.6 9.4 33.9 0l28.3-28.3c9.4-9.4 9.4-24.6.1-34zM208 336c-70.7 0-128-57.2-128-128 0-70.7 57.2-128 128-128 70.7 0 128 57.2 128 128 0 70.7-57.2 128-128 128z"/></svg>`;
 
-    // sample of a query list
-    const SEARCH_QUERY_URLS = [
-        {
-            "id": "google",
-            "name": "Google",
-            "url": "https://google.com/search"
-        },
+    // kinda self-explanatory, right?
+    function applySVG(svg, node) {
+        node.innerHTML = svg;
+    }
 
-        {
-            "id": "duckduckgo",
-            "name": "DuckDuckGo",
-            "url": "https://duckduckgo.com/"
-        },
+    const DB_URL_INPUT = document.querySelector("#dbUrlInput");
+    const DB_URL_SEARCH = document.querySelector("#dbUrlSearch");
+    let CURRENT_URL;
+    applySVG(SVG, DB_URL_SEARCH);
 
-        {
-            "id": "mdn",
-            "name": "MDN Web Docs",
-            "url": "https://developer.mozilla.org/en-US/search"
-        },
-
-        {
-            "id": "devdocs",
-            "name": "DevDocs",
-            "url": "https://devdocs.io/search"
-        },
-
-        {
-            "id": "gitlab",
-            "name": "GitLab",
-            "url": "https://gitlab.com/search",
-            "param": "search"
-        },
-
-        {
-            "id": "cdnjs",
-            "name": "cdnjs",
-            "url": "https://cdnjs.com/"
-        },
-
-        {
-            "id": "github",
-            "name": "GitHub",
-            "url": "https://github.com/search"
-        },
-
-        {
-            "id": "stackoverflow",
-            "name": "Stack Overflow",
-            "url": "https://stackoverflow.com/search"
-        },
-
-        {
-            "id": "npm",
-            "name": "npm Package Search",
-            "url": "https://npmjs.com/search"
+    function getListFromJSON(URL) {
+        fetch(URL)
+            .then(response => {
+                CURRENT_URL = response.url;
+                console.log(response.url)
+                return response;
+            })
+            .then(response => response.json())
+            .then(json => {
+                const SE_DATA_LIST = json;
+                SE_DATA_LIST.sort(sortID);
+                createList(SE_DATA_LIST);
+                console.log(SE_DATA_LIST);
+            })
+    }
+   
+    DB_URL_SEARCH.addEventListener("click", (event) => {
+        if (!DB_URL_INPUT.value) getListFromJSON("./se-list.json");
+        else {
+            getListFromJSON(DB_URL_INPUT.value)
+            DB_URL_INPUT.value = '';
         }
-    ]
-
-    SEARCH_QUERY_URLS.sort(sortID);
+    })
 
     function sortID(current, next) {
         if (current.id > next.id) return 1
@@ -70,8 +45,7 @@ function main() {
     }
 
     function createList(querylist) {
-        const SE_LIST = document.createElement("ul");
-        SE_LIST.setAttribute("id", "search-list");
+        while (SE_LIST.firstElementChild) {SE_LIST.removeChild(SE_LIST.firstElementChild)}
 
         function openSearchPage(eventListener) {
             const TARGET_INPUT = document.querySelector(`input#${obj.id}`);
@@ -124,7 +98,7 @@ function main() {
             const SE_INPUT_BTN = document.createElement("button");
             SE_INPUT_BTN.setAttribute("type", "button");
             SE_INPUT_BTN.setAttribute("class", "search-engine-input-button");
-            SE_INPUT_BTN.innerHTML = SVG;
+            applySVG(SVG, SE_INPUT_BTN)
             SE_INPUT_BTN.addEventListener("click", (event) => {
                 const TARGET_INPUT = document.querySelector(`input#${obj.id}`);
 
@@ -154,8 +128,6 @@ function main() {
 
         LIST_CONTAINER.appendChild(SE_LIST);
     }
-
-    createList(SEARCH_QUERY_URLS);
 }
 
 main();
