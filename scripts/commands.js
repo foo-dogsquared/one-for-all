f4c2cfe0c8e70c9ff5a117bfcab6ef11a615bb9e
foo-dogsquared <christiangabrielarazas@gmail.com>
2019-01-08 22:19:27 +0800

    Finish --(s) command

--- scripts/commands.js
+++ scripts/commands.js
@@ -6,11 +6,38 @@ const ONE_FOR_ALL_TOGGLE_DB_AT_START = `fds_one-for-all_toggle_db_at_start`;
 const urlRegex = /(?:https?|file|ftp)\:\/\/[\w|\W|\d]+[.][\w]+/;
 
 function setKeyword(command, wholestring = DB_URL_INPUT.value) {
-    
+    const splitArguments = wholestring.split(/\s+/);
+    const commandIndex = splitArguments.indexOf(command);
+    const potentialArguments = splitArguments.slice(commandIndex + 1);
+    let textMessage = "";
+    const validArgumentList = [];
+    for (const argument of potentialArguments) {
+        const validArgument = argument.match(/(\w+)=\"(.+)\"/);
+        const potentialCommandParameter = argument.match(/--\$\w+/);
+        if (!validArgument && !potentialCommandParameter)
+            textMessage += `<code style="font-size:1.1em;">${command}</code>: Invalid argument format given at index ${wholestring.indexOf(argument)}.<br>`;
+        else if (potentialCommandParameter) {
+            if (validArgumentList.length <= 0) textMessage = `<code style="font-size:1.1em;">${command}</code>: No value detected.<br>`;
+            break;
+        }
+        else {
+            if (!validArgument[2].match(urlRegex)) {
+                textMessage += `<code style="font-size:1.1em;">${command}</code>: Given argument value of ${validArgument[1]} is not a valid URL.<br>`;
+                continue;
+            }
+            else {
+                textMessage += `<code style="font-size:1.1em;">${command}</code>: Keyword <u>${validArgument[1]}</u> has been ${(localStorage.getItem(ONE_FOR_ALL_KEYWORD_TEMPLATE + encodeURIComponent(validArgument[1]))) ? "replaced" : "added"} with the value <u>${validArgument[2]}</u>.<br>`;
+                localStorage.setItem(`${ONE_FOR_ALL_KEYWORD_TEMPLATE}${encodeURIComponent(validArgument[1])}`, encodeURIComponent(validArgument[2]));
+                validArgumentList.push(validArgument);
+            }
+        }
+    }
+
+    return textMessage;
 }
 
 function removeKeyword(command, wholestring = DB_URL_INPUT.value) {
-    
+
 }
 
 function setDefault(command, wholestring = DB_URL_INPUT.value) {
