307da5ad8a29d651ef58b9678a43a7eaae2a16a9
foo-dogsquared <christiangabrielarazas@gmail.com>
2019-01-09 09:53:02 +0800

    Finish  commands

--- scripts/commands.js
+++ scripts/commands.js
@@ -5,53 +5,99 @@ const ONE_FOR_ALL_DEFAULT_DB = `fds_one-for-all_default_db`;
 const ONE_FOR_ALL_TOGGLE_DB_AT_START = `fds_one-for-all_toggle_db_at_start`;
 const urlRegex = /(?:https?|file|ftp)\:\/\/[\w|\W|\d]+[.][\w]+/;
 
+function formatCommandTextMessage(commandName, message, status = "text") {return `<code style="${(status === "error") ? "color:red" : "color:black"};font-size:1.1em;">${commandName}</code>: ${message}<br>`;}
+
 function setKeyword(command, wholestring = DB_URL_INPUT.value) {
     const splitArguments = wholestring.split(/\s+/);
     const commandIndex = splitArguments.indexOf(command);
     const potentialArguments = splitArguments.slice(commandIndex + 1);
     let textMessage = "";
     const validArgumentList = [];
+    const urlRegex = /^(https?|file|ftp)\:\/\/[\w|\W|\d]+[.][\w]+$/gi;
     for (const argument of potentialArguments) {
-        const validArgument = argument.match(/(\w+)=\"(.+)\"/);
+        const validArgument = argument.match(/([A-Za-z0-9_-])=\"(.+)\"/);
         const potentialCommandParameter = argument.match(/--\$\w+/);
         if (!validArgument && !potentialCommandParameter)
-            textMessage += `<code style="font-size:1.1em;">${command}</code>: Invalid argument format given at index ${wholestring.indexOf(argument)}.<br>`;
+            textMessage += formatCommandTextMessage(command,`Invalid argument format given at index ${wholestring.indexOf(argument)}.`);
         else if (potentialCommandParameter) {
-            if (validArgumentList.length <= 0) textMessage = `<code style="font-size:1.1em;">${command}</code>: No value detected.<br>`;
             break;
         }
         else {
-            if (!validArgument[2].match(urlRegex)) {
-                textMessage += `<code style="font-size:1.1em;">${command}</code>: Given argument value of ${validArgument[1]} is not a valid URL.<br>`;
-                continue;
-            }
+            if (!validArgument[2].match(urlRegex)) textMessage += formatCommandTextMessage(command,`Given argument value of ${validArgument[1]} is not a valid URL.`);
             else {
-                textMessage += `<code style="font-size:1.1em;">${command}</code>: Keyword <u>${validArgument[1]}</u> has been ${(localStorage.getItem(ONE_FOR_ALL_KEYWORD_TEMPLATE + encodeURIComponent(validArgument[1]))) ? "replaced" : "added"} with the value <u>${validArgument[2]}</u>.<br>`;
+                textMessage += formatCommandTextMessage(command,`Keyword <u>${validArgument[1]}</u> has been ${(localStorage.getItem(ONE_FOR_ALL_KEYWORD_TEMPLATE + encodeURIComponent(validArgument[1]))) ? "replaced" : "added"} with the value <u>${validArgument[2]}</u>.`);
                 localStorage.setItem(`${ONE_FOR_ALL_KEYWORD_TEMPLATE}${encodeURIComponent(validArgument[1])}`, encodeURIComponent(validArgument[2]));
-                validArgumentList.push(validArgument);
             }
+            validArgumentList.push(validArgument);
         }
     }
+
+    if (validArgumentList.length <= 0) textMessage += formatCommandTextMessage(command,`No value detected.`);
 
     return textMessage;
 }
 
 function removeKeyword(command, wholestring = DB_URL_INPUT.value) {
-    
+    const splitArguments = wholestring.split(/\s+/);
+    const commandIndex = splitArguments.indexOf(command);
+    const potentialArguments = splitArguments.slice(commandIndex + 1);
+    let textMessage = "";
+    const validArgumentList = [];
+    for (const argument of potentialArguments) {
+        const potentialCommandParameter = argument.match(/--\$\w+/);
+        const keywordArray = argument.match(/\[.+?\]/);
+        console.log(keywordArray);
+        if (!argument || potentialCommandParameter) break;
+        else if (keywordArray) {
+            const keywordsStr = keywordArray.join();
+            const keywords = keywordsStr.substr(1, keywordsStr.length - 2).split(",");
+            for (const keyword of keywords) {
+                if (keyword.match(/[A-Za-z0-9_-]/)) {
+                    if (!localStorage.getItem(`${ONE_FOR_ALL_KEYWORD_TEMPLATE}${keyword}`)) textMessage += formatCommandTextMessage(command, `Keyword ${keyword} is not yet set.`); 
+                    else {
+                        localStorage.removeItem(`${ONE_FOR_ALL_KEYWORD_TEMPLATE}${keyword}`);
+                        textMessage += formatCommandTextMessage(command, `Usable keyword ${keyword} has been removed.`);
+                    }
+                }
+                else textMessage += formatCommandTextMessage(command, `Only accepts phrases with at least one alphanumeric character, hypens, and underscores.`);
+            }
+            validArgumentList.push(keywordArray);
+        }
+        else if (argument.match(/[A-Za-z0-9_-]/)) {
+            if (!localStorage.getItem(`${ONE_FOR_ALL_KEYWORD_TEMPLATE}${argument}`)) textMessage += formatCommandTextMessage(command, `Keyword ${argument} is not yet set.`); 
+            else {
+                localStorage.removeItem(`${ONE_FOR_ALL_KEYWORD_TEMPLATE}${argument}`);
+                textMessage += formatCommandTextMessage(command, `Usable keyword ${argument} has been removed.`);
+            }
+            validArgumentList.push(argument);
+        }
+        else textMessage += formatCommandTextMessage(command, `Only accepts phrases with at least one alphanumeric character, hypens, and underscores.`);
+    }
+
+    if (validArgumentList.length <= 0) textMessage += formatCommandTextMessage(command, "No value detected.");
+
+    return textMessage;
 }
 
 function setDefault(command, wholestring = DB_URL_INPUT.value) {
     const splitArguments = wholestring.split(/\s+/);
     const commandIndex = splitArguments.indexOf(command);
     const potentialArgument = splitArguments[commandIndex + 1];
+    const urlRegex = /^(https?|file|ftp)\:\/\/[\w|\W|\d]+[.][\w]+$/gi;
     let textMessage = "";
-    if (potentialArgument.match(urlRegex)) {
+    if (!potentialArgument || potentialArgument.match(/--\$\w+/) ) textMessage += formatCommandTextMessage(command,`No value detected.`);
+    else if (potentialArgument.match(urlRegex)) {
         localStorage.setItem(ONE_FOR_ALL_DEFAULT_DB, potentialArgument);
-        textMessage += `<code style="font-size:1.1em;">${command}</code>: Default database is set at ${potentialArgument}.<br>`;
+        textMessage += formatCommandTextMessage(command,`Default database is set at ${potentialArgument}.`);
     }
-    else if (potentialArgument === "clear") localStorage.removeItem(ONE_FOR_ALL_DEFAULT_DB);
-    else if (potentialArgument.match(/--\$\w+/) || !potentialArgument) textMessage += `<code style="font-size:1.1em;">${command}</code>: No value detected.<br>`;
-    else textMessage += `<code style="font-size:1.1em;">${command}</code>: Given value is invalid URL.<br>`;
+    else if (potentialArgument === "-clear") {
+        if (localStorage.getItem(ONE_FOR_ALL_DEFAULT_DB)) {
+            localStorage.removeItem(ONE_FOR_ALL_DEFAULT_DB);
+            textMessage += formatCommandTextMessage(command, `: Default database setting is cleared. Default database URL is at "./se-list.json".`);
+        } 
+        else textMessage += formatCommandTextMessage(command, `: There is no default database stored.`);
+    }
+    else textMessage += formatCommandTextMessage(command,`Given value is invalid URL.`);
 
     return textMessage;
 }
@@ -61,12 +107,13 @@ function toggleShowList(command, wholestring = DB_URL_INPUT.value) {
     const commandIndex = splitArguments.indexOf(command);
     const potentialArgument = splitArguments[commandIndex + 1];
     let textMessage = "";
-    if (potentialArgument === "true" || potentialArgument === "false") {
+    if (!potentialArgument) textMessage += formatCommandTextMessage(command, `No value detected.`);
+    else if (potentialArgument === "true" || potentialArgument === "false") {
         localStorage.setItem(ONE_FOR_ALL_TOGGLE_DB_AT_START, potentialArgument);
-        textMessage += `<code style="font-size:1.1em;">${command}</code>: Toggling retrieving databases at start is set at ${potentialArgument}.<br>`;
+        textMessage += formatCommandTextMessage(command,`Toggling retrieving databases at start is ${(potentialArgument === "true") ? "enabled" : "disabled"}.`);
     }
-    else if (potentialArgument.match(/--\$\w+/) || !potentialArgument) textMessage += `<code style="font-size:1.1em;">${command}</code>: No value detected.<br>`;
-    else textMessage += `<code style="font-size:1.1em;">${command}</code>: Toggling database at start only have <code>true</code> and <code>false</code> as possible values.<br>`;
+    else if (potentialArgument.match(/--\$\w+/) || !potentialArgument) textMessage += formatCommandTextMessage(command,`No value detected.`);
+    else textMessage += formatCommandTextMessage(command,`Toggling database at start only have <code>true</code> and <code>false</code> as possible values.`);
 
     return textMessage;
 }
